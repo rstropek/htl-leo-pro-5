82869969e7e2b1de8368c062335e5f7827651fb1
Rainer Stropek <rainer@software-architects.at>
2022-03-02 09:04:00 +0100

    Add payments controller

---
+++ homeworks/0700-city-congestion-charge/sample-solution/CityCongestionCharge.Data/FilteredPayments.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CityCongestionCharge.Data;
+
+public partial class CccDataContext
+{
+    public IQueryable<Payment> FilteredPayments(string? licensePlateFilter,
+        bool? onlyFuturePayments, bool? onlyAnonymous)
+    {
+        IQueryable<Payment> payments = Payments.Include(p => p.Car);
+
+        if (licensePlateFilter != null)
+        {
+            payments = payments.Where(p => p.Car!.LicensePlate.Contains(licensePlateFilter));
+        }
+
+        if (onlyFuturePayments is true)
+        {
+            payments = payments.Where(p => p.PaidForDate > DateTime.Today);
+        }
+
+        if (onlyAnonymous is true)
+        {
+            payments = payments.Where(p => p.PayingPerson == null);
+        }
+
+        return payments;
+    }
+}
